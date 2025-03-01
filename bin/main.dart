a272564c516c76ec53ad8274a345c29e9a45b168
YumNumm <onory0917@gmail.com>
2025-03-01 17:03:07 +0900

    うごいた〜〜〜!

--- bin/main.dart
+++ bin/main.dart
@@ -1,18 +1,34 @@
 import 'dart:js_interop';
 
-// JSResponseを使いたいので cf_workers を入れています．
-import 'package:cf_workers/src/http.dart';
-
-@JS()
-external JSPromise<JSResponse> fetch(JSString url);
-
-// Dartの結果をresultを通して渡しています
-@JS('__js.result')
-external void result(JSResponse response);
+import 'package:cf_workers/cf_workers.dart';
+import 'package:cf_workers/http.dart';
+import 'package:http/http.dart';
 
 void main(List<String> arguments) async {
-  final JSResponse resp =
-      await fetch('https://httpbin.org/anything'.toJS).toDart;
-  print(resp);
-  result(resp);
+  return Workers((request) async {
+    final req = await request.toDart;
+    print(req.url);
+    print(req.toString());
+
+    final jsRes =
+        await http("https://httpbin.org/anything", "GET", null, null).toDart;
+    final res = await jsRes.toDart;
+    print("----- HTTP RESPONSE (handled by Dart Worker) -----");
+    print(res.body);
+    print(res.statusCode);
+    print(res.headers);
+    return Response.bytes(
+      res.bodyBytes,
+      res.statusCode,
+      headers: res.headers,
+    ).toJS;
+  }).serve();
 }
+
+@JS('__dart_cf_workers.http')
+external JSPromise<JSResponse> http(
+  String url,
+  String method, [
+  JSBoxedDartObject? headers,
+  String? body,
+]);
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
+import type { ExecutionContext } from "@cloudflare/workers-types";
 import { instantiate, invoke } from "./__dart/main.mjs";
 import mod from "./__dart/main.wasm";
-import { ExecutionContext } from "@cloudflare/workers-types";
 
 export default {
-  async fetch(
-    request: Request,
-    env: never,
-    ctx: ExecutionContext,
-  ): Promise<Response> {
-    let responseMessage: string;
-    const instance = await instantiate(mod, Promise.resolve({}));
+	async fetch(
+		request: Request,
+		env: never,
+		ctx: ExecutionContext,
+	): Promise<Response> {
+		const dartInstance = await instantiate(mod);
 
-    return new Promise((resolve) => {
-      // Dart側で発生したPromiseをinvokeで受け持つことが現状できないので，このような作りにしています．
-      invoke(instance);
-      (globalThis as any).__js = {
-        result(response: Response) {
-          resolve(response);
-        },
-      };
-    });
-  },
+		return new Promise((resolve) => {
+			globalThis.__dart_cf_workers = {
+				request: () => request,
+				response: (res) => resolve(res),
+				http: async (
+					url: string,
+					method: string,
+					headers?: object,
+					body?: string,
+				) => {
+					try {
+						console.log("----- HTTP REQUEST (handled by JS Worker) -----");
+						console.log({
+							message: "Sending request",
+							url,
+							method,
+							headers,
+							body,
+						});
+						const res = await fetch(url, {
+							method,
+							body,
+						});
+						console.log({
+							message: "Response received",
+							status: res.status,
+							headers: res.headers,
+						});
+						return res;
+					} catch (e) {
+						console.error(e);
+						return {
+							status: 500,
+							body: e.message,
+						};
+					}
+				},
+			};
+			dartInstance();
+		});
+	},
 };
